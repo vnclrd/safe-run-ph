6ab40856410f81be97707c63d659333b933a65b1
Miguel Ivan Calarde <miguel.calarde@gmail.com>
2026-04-03 19:57:13 +0800

    test: test deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,15 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
-
-  /* IMPORTANT: If your site is at vnclrd.github.io/safe-run-ph/, 
-    uncomment the line below and change it to your repo name. 
-    If you use a custom domain, you don't need this.
-  */
-  // basePath: "/safe-run-ph", 
+  images: { unoptimized: true },
+  basePath: "/safe-run-ph", 
 };
 
 export default nextConfig;
