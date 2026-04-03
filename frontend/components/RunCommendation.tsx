127b9e339995faef1ba72e6b5f992700672a30a2
Miguel Ivan Calarde <miguel.calarde@gmail.com>
2026-04-03 19:38:45 +0800

    style: arrange styles on cards

--- frontend/components/RunCommendation.tsx
+++ frontend/components/RunCommendation.tsx
@@ -26,26 +26,29 @@ export default function RunCommendation({
   }
 
   return (
-    <div className="w-full h-auto min-h-[18.75rem] md:h-75 flex flex-col justify-between p-6 sm:p-8 rounded-[2rem] bg-white border border-slate-100 shadow-sm relative overflow-hidden transition-all duration-700">
+    <div className="w-full h-auto min-h-[18.75rem] md:h-75 flex flex-col p-6 sm:p-8 rounded-[2rem] bg-white border border-slate-100 shadow-sm relative overflow-hidden transition-all duration-700">
       <div
-        className={`relative z-10 flex flex-col h-full transition-all duration-1000 ease-out transform ${!recommendation ? "opacity-0 translate-y-4" : "opacity-100 translate-y-0"}`}
+        className={`relative z-10 flex flex-col h-full items-center sm:items-start text-center sm:text-left transition-all duration-1000 ease-out transform ${
+          !recommendation ? "opacity-0 translate-y-4" : "opacity-100 translate-y-0"
+        }`}
       >
-        <div>
-          <p className="text-[10px] font-black text-slate-400 uppercase tracking-[0.2em]">
-            RunCommendation
-          </p>
+        <p className="w-full text-[10px] font-black text-slate-400 uppercase tracking-[0.2em]">
+          RunCommendation
+        </p>
+
+        <div className="flex-grow flex flex-col justify-center items-center sm:items-start">
           <h2
-            className={`text-3xl sm:text-5xl md:text-5xl font-black tracking-tight leading-tight mt-2 md:mt-4 line-clamp-2 ${status.textColor}`}
+            className={`text-4xl sm:text-5xl md:text-4xl font-black tracking-tight leading-tight mt-2 sm:mt-4 line-clamp-2 ${status.textColor}`}
           >
             {recommendation?.title}
           </h2>
-        </div>
-        <div className="flex-grow flex items-center py-4 md:py-0">
-          <p className="md:mt-[-24] text-sm sm:text-base font-bold text-slate-600 leading-relaxed line-clamp-4 md:line-clamp-3">
+          
+          <p className="h-35 text-md sm:text-lg font-bold text-slate-600 leading-relaxed mt-6 sm:mt-6 md:mt-6 md:line-clamp-3">
             {recommendation?.message}
           </p>
         </div>
       </div>
+
       <div
         className={`absolute bottom-0 left-0 w-full h-2 bg-gradient-to-r transition-opacity duration-1000 ease-out ${
           !recommendation ? "opacity-0" : "opacity-100"
